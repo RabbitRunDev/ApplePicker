e7db4523c3c4f73a54f976ccda7d21850bd293b1
Aidan Cushing <aac202@rutgers.edu>
2020-02-05 21:08:10 -0500

    Added Direction Changes, fixed bug

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 10f;
+
+    //Distance where AppleTree turns around
+    public float leftAndRightEdge = 20f;
+
+    // Chance that the AppleTree turns around
+    public float chanceToChangeDirections = 0.1f;
+
+    //Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
     // Start is called before the first frame update
     void Start()
+    // Dropping Apples every second
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        //	Changing	Direction
+
+        // Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            //Move Right
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            //Move Left
+            speed = -Mathf.Abs(speed);
+        }
+        else if (Random.value < chanceToChangeDirections)
+        {
+            //Change Direction
+            speed *= -1;
+        }
     }
 }
