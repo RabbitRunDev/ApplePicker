33b6a07ddd06c0a2be802a62304f4b39c45569a7
Aidan Cushing <aac202@rutgers.edu>
2020-02-06 19:22:41 -0500

    Cube Spawner Script implemented

---
+++ Assets/Scripts/CubeInstancer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeInstancer : MonoBehaviour
+{
+
+    public GameObject cube;
+    public int rows;
+    public int columns;
+    public int spacing;
+
+    private Vector3 instancePos = new Vector3(0.0f, 0.0f, 0.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int r = 0; r < rows; r++)
+        {
+            instancePos.z = 0;
+            instancePos.x += spacing;
+            for (int c = 0; c < columns; c++)
+            {
+                Instantiate(cube);
+                cube.transform.position = instancePos;
+                instancePos.z += spacing;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
